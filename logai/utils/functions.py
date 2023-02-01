fa74f614746a8560c7685672e627f33ad6f5f401
Ikko Eltociear Ashimine <eltociear@gmail.com>
2023-02-01 17:21:09 +0900

    fix typo in functions.py
    Covert -> Convert

--- logai/utils/functions.py
+++ logai/utils/functions.py
@@ -77,7 +77,7 @@ def get_parameter_list(row):
 
 def pd_to_timeseries(log_features: pd.Series):
     """
-    Covert pandas.DataFrame to merlion.TimeSeries for log counter vectors.
+    Convert pandas.DataFrame to merlion.TimeSeries for log counter vectors.
 
     :param log_features: log feature dataframe must only contain two columns
       ['timestamp': datetime, constants.LOGLINE_COUNTS: int].
